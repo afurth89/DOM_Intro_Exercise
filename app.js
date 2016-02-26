073fe9087d716628834bc1fb5428fe59a12fc338
Andrew Furth <andrew.furth@gmail.com>
2016-02-25 18:04:56 -0800

    complete plus bonus - enter the konami code and see

--- app.js
+++ app.js
@@ -85,3 +85,27 @@ window.addEventListener('keypress', function(e) {
 
 // BONUS: If someone types the Konami Code, the page alerts 
 // "YOU ARE AN EVENT HANDLER GURUUUUUUUUU!"
+
+// Found this from: http://mattkirman.com/how-to-recreate-the-konami-code-in-javascript/
+
+// check to make sure that the browser can handle window.addEventListener
+if (window.addEventListener) {
+    // create the keys and konami variables
+    var keys = [],
+        konami = "38,38,40,40,37,39,37,39,66,65";
+    
+    // bind the keydown event to the Konami function
+    window.addEventListener("keydown", function(e){
+        // push the keycode to the 'keys' array
+        keys.push(e.keyCode);
+        
+        // and check to see if the user has entered the Konami code
+        if (keys.toString().indexOf(konami) >= 0) {
+            // do something such as:
+            // alert('Konami');
+            alert("YOU ARE AN EVENT HANDLER GURUUUUUUUUU!");
+            // and finally clean up the keys array
+            keys = [];
+        };
+    }, true);
+};
